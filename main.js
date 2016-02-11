8fdf577ffbad9420afdeb079851c503057ebf12b
Zack Winchell <zackwinchell@gmail.com>
2016-02-10 22:18:13 -0800

    made partials references relative for gh-pages

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var app = angular.module("angularStocks", ["ui.router"]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state("home", {url: "/", templateUrl: "/partials/home.html"})
-    .state("list", {url: "/list", templateUrl: "/partials/list.html", controller: "listCtrl"})
-    .state("add", {url: "/add", templateUrl: "/partials/add.html", controller: "addCtrl"});
+    .state("home", {url: "/", templateUrl: "./partials/home.html"})
+    .state("list", {url: "/list", templateUrl: "./partials/list.html", controller: "listCtrl"})
+    .state("add", {url: "/add", templateUrl: "./partials/add.html", controller: "addCtrl"});
 
     $urlRouterProvider.otherwise("/");
 });
